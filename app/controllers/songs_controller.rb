bde2767dd62d46e1a3dcf3411676bdd1a8d3d52e
Carley Tripp <carley.tripp@outlook.com>
2019-03-18 02:32:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     @artist = Artist.find_by(id: params[:artist_id])
     
-    if @artist
+    if @artist != nil
       @songs = @artist.songs
     else
       redirect_to artists_path
