5bb44f42b5a019cc98c08face3a07f953b0b3846
Carley Tripp <carley.tripp@outlook.com>
2019-03-18 02:54:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,13 +11,11 @@ class SongsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find_by(id: params[:artist_id])
-    @song = Song.find_by(id: params[:id])
+    @song = Song.find(params[:id])
 
-    if !@artist.songs.include(@song)
-      redirect_to artist_songs_path(params[:artist_id])
-      flash[:alert] = "Song not found."
-    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to artist_songs_path(params[:artist_id])
+    flash[:alert] = "Song not found."
   end
 
   def new
