e2ecad9cf7816f97b70cccb8b6c2101c27b77f15
Carley Tripp <carley.tripp@outlook.com>
2019-03-18 03:07:44 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     @artist = Artist.find_by(id: params[:artist_id])
     
-    if @artist.nil? || params[:artist_id].nil?
+    if @artist.nil? || params[:artist_id].empty?
       redirect_to artists_path
       flash[:alert] = "Artist not found."
     else
